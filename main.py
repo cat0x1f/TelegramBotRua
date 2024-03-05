26bbb3e7f73642eaa915c572af0181cbd605d218
SenYue <sencat31@outlook.com>
2024-03-05 23:55:33 +0800

    edit

--- main.py
+++ main.py
@@ -39,26 +39,26 @@ async def repeat_repeat_repeat(message):
             await bot.reply_to(message, reply)
 
 
-async def jrrp_text_init(score):
-    nub = int(score)
+async def jrrp_text_init(nub_in):
+    nub = int(nub_in)
     if nub == 100:
-        return "今天的人品是：" + str(score) + "\n" + "100 人品好评!!!"
+        return "今天的人品是：" + str(nub_in) + "\n" + "100 人品好评!!!"
     elif nub >= 90:
-        return "今天的人品是：" + str(score) + "\n" + "今天的人品非常不错呢"
+        return "今天的人品是：" + str(nub_in) + "\n" + "今天的人品非常不错呢"
     elif nub >= 70:
-        return "今天的人品是：" + str(score) + "\n" + "哇,人品还挺好的!"
+        return "今天的人品是：" + str(nub_in) + "\n" + "哇,人品还挺好的!"
     elif nub >= 60:
-        return "今天的人品是：" + str(score) + "\n" + "今天是 非常¿ 不错的一天呢!"
+        return "今天的人品是：" + str(nub_in) + "\n" + "今天是 非常¿ 不错的一天呢!"
     elif nub > 50:
-        return "今天的人品是：" + str(score) + "\n" + "你的人品还不错呢"
+        return "今天的人品是：" + str(nub_in) + "\n" + "你的人品还不错呢"
     elif nub == 50:
-        return "今天的人品是：" + str(score) + "\n" + "五五开！"
+        return "今天的人品是：" + str(nub_in) + "\n" + "五五开！"
     elif nub >= 40:
-        return "今天的人品是：" + str(score) + "\n" + f"还好还好只有 {nub}"
+        return "今天的人品是：" + str(nub_in) + "\n" + f"还好还好只有 {nub}"
     elif nub >= 20:
-        return "今天的人品是：" + str(score) + "\n" + f"{nub} 这数字太....要命了"
+        return "今天的人品是：" + str(nub_in) + "\n" + f"{nub} 这数字太....要命了"
     elif nub >= 0:
-        return "今天的人品是：" + str(score) + "\n" + "抽大奖¿"
+        return "今天的人品是：" + str(nub_in) + "\n" + "抽大奖¿"
 
 
 async def from_input_get_score(user_id):
